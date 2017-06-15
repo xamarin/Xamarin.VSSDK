4cc9fc43506b79d69a0d1bf21ff2aed8acc10ab3
Daniel Cazzulino <daniel@cazzulino.com>
2017-06-15 14:02:37 -0300

    Update to latest stable 15.3 packages

--- test/Xamarin.SampleVsix/SamplePackage.cs
+++ test/Xamarin.SampleVsix/SamplePackage.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
 using Xamarin.SampleVsix.Properties;
 
 namespace Xamarin.SampleVsix
 {
-    public class SamplePackage
+    [Guid("BC1550C5-64C9-4B9A-A4F6-7D477C9178F1")]
+    [InstalledProductRegistration("Sample", "Sample", "1.0 (asdf)")]
+    public class SamplePackage : Package
     {
         //public override string ToString()
         //{
