d268c81747b09d97a3686ac6acd58296201bd808
Daniel Cazzulino <daniel@cazzulino.com>
2017-06-16 15:11:37 -0300

    Remove unnecessary Immutable.10 on Dev=15

--- test/Xamarin.SampleVsix/SamplePackage.cs
+++ test/Xamarin.SampleVsix/SamplePackage.cs
@@ -3,10 +3,14 @@ using System.Runtime.InteropServices;
 using Microsoft.VisualStudio.Shell;
 using Xamarin.SampleVsix.Properties;
 
+[assembly: ProvideCodeBase]
+
 namespace Xamarin.SampleVsix
 {
     [Guid("BC1550C5-64C9-4B9A-A4F6-7D477C9178F1")]
     [InstalledProductRegistration("Sample", "Sample", "1.0 (asdf)")]
+    [PackageRegistration(RegisterUsing = RegistrationMethod.CodeBase, UseManagedResourcesOnly = true)]
+    [ProvideBindingPath]
     public class SamplePackage : Package
     {
         //public override string ToString()
