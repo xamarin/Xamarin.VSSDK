7bf5689d6f6c47b91b2b1be5daa056deaf34ea4d
Adrian Alonso <adrianalonso@gmail.com>
2017-08-10 14:19:23 -0300

    Override GeneratePkgDef target
    In order to filter the unnecessary assembly references passed to GenPkgDef.exe to
    avoid errors with max length path (probably common when using PackageReference)

--- test/Xamarin.VSSDK.Tests/GeneratePkgDefTests.cs
+++ test/Xamarin.VSSDK.Tests/GeneratePkgDefTests.cs
@@ -1,24 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Reflection;
 using Microsoft.Build.Evaluation;
 using Microsoft.Build.Execution;
-using System.Linq;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace Xamarin.VSSDK.Tests
 {
-    public class DeployVsixExtensionFilesTests : VsTest
+    public class GeneratePkgDefTests : VsTest
     {
         ITestOutputHelper output;
 
-        public DeployVsixExtensionFilesTests(ITestOutputHelper output) : base(output)
+        public GeneratePkgDefTests(ITestOutputHelper output) : base(output)
         { }
 
-        [Fact]
-        public void ExtensionFilesAreDeployedWhenBuidingExtension()
+        //[Fact]
+        public void PkgDefFileIsGeneratedWhenBuidingExtensionWithGeneratePkgDefFilePropertySet()
         {
             var vsixDeploymentPath = GetVsixDeploymentPath();
             if (!string.IsNullOrEmpty(vsixDeploymentPath) && Directory.Exists(vsixDeploymentPath))
@@ -32,16 +30,14 @@ namespace Xamarin.VSSDK.Tests
                 { "VSSDKTargetPlatformRegRootSuffix", RootSuffix },
             }, "15.0", new ProjectCollection());
 
-            var result = Builder.Build(project, "Restore;Build", output: output);
+            var result = Builder.Build(project, "Restore;Rebuild", output: output);
 
             Assert.Equal(BuildResultCode.Success, result.BuildResult.OverallResult);
 
             vsixDeploymentPath = GetVsixDeploymentPath();
 
             Assert.True(Directory.Exists(vsixDeploymentPath));
-            Assert.True(File.Exists(Path.Combine(vsixDeploymentPath, "VsixTemplate.dll")), "VsixTemplate.dll not found");
             Assert.True(File.Exists(Path.Combine(vsixDeploymentPath, "VsixTemplate.pkgdef")), "VsixTemplate.pkgdef not found");
-            Assert.True(File.Exists(Path.Combine(vsixDeploymentPath, "extension.vsixmanifest")), "extension.vsixmanifest found");
         }
     }
 }
---
+++ test/Xamarin.VSSDK.Tests/VsTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Linq;
+using Xunit.Abstractions;
+
+namespace Xamarin.VSSDK.Tests
+{
+    public class VsTest
+    {
+#if Dev14
+        const string BaseRootSuffix = "XamarinVSSDK_Tests14.0_";
+#elif Dev15
+        const string BaseRootSuffix = "XamarinVSSDK_Tests15.0_";
+#elif Dev16
+        protected const string BaseRootSuffix = "XamarinVSSDK_Tests16.0_";
+#elif Dev17
+        const string BaseRootSuffix = "XamarinVSSDK_Tests17.0_";
+#endif
+
+#if Dev14
+        protected const string TargetFramework = "net461";
+#else
+        protected const string TargetFramework = "net462";
+#endif
+
+
+        ITestOutputHelper output;
+
+        public VsTest(ITestOutputHelper output)
+        {
+            this.output = output;
+
+#if Dev14
+            Environment.SetEnvironmentVariable("VsSDKToolsPath", Path.Combine(Directory.GetCurrentDirectory(), "bin"));
+#endif
+
+#if Dev15
+            Assembly
+                .LoadFrom("Microsoft.VisualStudio.Sdk.BuildTasks.15.0.dll")
+                .GetType("Microsoft.VisualStudio.Sdk.BuildTasks.FolderLocator")
+                .GetProperty("InstanceInstallationPath", BindingFlags.Static | BindingFlags.Public)
+                .SetValue(null, Environment.GetEnvironmentVariable("VSINSTALLDIR"));
+#endif
+        }
+
+        protected string RootSuffix => BaseRootSuffix;
+
+        string GetTargetInstancePath() =>
+            Directory
+                .EnumerateDirectories(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Microsoft", "VisualStudio"))
+                .FirstOrDefault(x => x.Contains(RootSuffix));
+
+        protected string GetVsixDeploymentPath()
+        {
+            var targetInstancePath = GetTargetInstancePath();
+            if (!string.IsNullOrEmpty(targetInstancePath))
+                return Path.Combine(targetInstancePath, "Extensions", "Xamarin", "Xamarin.VSSDK.Test", "1.0.0");
+
+            return null;
+        }
+    }
+}
