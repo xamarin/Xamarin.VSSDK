67717a94cb846efe93b8adc9a7d9f76213d74adb
Daniel Cazzulino <daniel@cazzulino.com>
2017-12-12 09:12:32 -0300

    Add support for generating binding redirects
    Usage examples:
    
    ```
        <BindingRedirected Include="Newtonsoft.Json">
          <!-- Showcases specifying only From metadata -->
          <From>6.0.0.0</From>
        </BindingRedirected>
        <!-- Showcases specifying no From/To metadata at all -->
        <BindingRedirected Include="AutoMapper" />
        <!-- Showcases matching a specific assembly -->
        <BindingRedirected Include="^System, Version=4.0.0.0" From="2.0.0.0" />
        <!-- Showcases matching with a regex that matches two assemblies -->
        <BindingRedirected Include="System.Xml.+" From="3.0.0.0" />
    ```
    
    The only configurable property is:
    
    ```
      <PropertyGroup>
        <!-- Causes default old version from to be 99.9.9.9 -->
        <BindingRedirectAllVersions>true</BindingRedirectAllVersions>
      </PropertyGroup>
    ```
    
    Pending some tests, but manual testing verified.

---
+++ test/Xamarin.VSSDK.Tests/GenerateBindingRedirectsTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Build.Evaluation;
+using Microsoft.Build.Execution;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Xamarin.VSSDK.Tests
+{
+    public class GenerateBindingRedirectsTests : VsTest
+    {
+        // This resolves the SDKs to the same location that was used to compile this project.
+        static GenerateBindingRedirectsTests() => Environment.SetEnvironmentVariable(
+            nameof(ThisAssembly.Project.Properties.MSBuildSDKsPath),
+            ThisAssembly.Project.Properties.MSBuildSDKsPath);
+
+        public GenerateBindingRedirectsTests(ITestOutputHelper output) : base(output) { }
+
+        //[InlineData("net461")]
+        //[InlineData("net462")]
+        //[Theory(Skip = "Can't make this work yet")]
+        [Fact]
+        public void BindingRedirectsCanBeProvided()
+        {
+            Func<ProjectInstance> factory = () => new ProjectInstance("BindRedirected.csproj", new Dictionary<string, string>
+            {
+                { "TargetFramework", "net461" },
+                { "Configuration", ThisAssembly.Project.Properties.Configuration },
+                { nameof(ThisAssembly.Project.Properties.MSBuildExtensionsPath), ThisAssembly.Project.Properties.MSBuildExtensionsPath },
+                { nameof(ThisAssembly.Project.Properties.NuGetRestoreTargets), ThisAssembly.Project.Properties.NuGetRestoreTargets },
+                { nameof(ThisAssembly.Project.Properties.CSharpCoreTargetsPath), ThisAssembly.Project.Properties.CSharpCoreTargetsPath },
+                { nameof(ThisAssembly.Project.Properties.RoslynTargetsPath), ThisAssembly.Project.Properties.RoslynTargetsPath },
+            }, "15.0", new ProjectCollection());
+
+            var result = Builder.Build(factory(), "Restore").AssertSuccess();
+            result = Builder.Build(factory(), "ReportBindingRedirects").AssertSuccess();
+        }
+    }
+}
